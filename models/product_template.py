32be65f6b93c42b7e310bc0c054957f098234b9f
yousseftarhri <yousseftarhri@gmail.com>
2025-08-14 16:02:43 +0100

    message quantity min

--- models/product_template.py
+++ models/product_template.py
@@ -11,3 +11,24 @@ class ProductTemplate(models.Model):
         if record.product_variant_count <= 1:
             record.product_variant_ids.minimum_qty = record.minimum_qty_tmpl
         return record
+
+    def write(self, vals):
+        res = super().write(vals)
+        for record in self:
+            if 'minimum_qty_tmpl' in vals and record.product_variant_count <= 1:
+                record.product_variant_ids.minimum_qty = record.minimum_qty_tmpl
+        return res
+
+
+class ProductTemplateMOQ(models.Model):
+    _inherit = 'product.template'
+
+    def _get_combination_info(self, *args, **kwargs):
+        res = super()._get_combination_info(*args, **kwargs)
+
+        product_id = res.get('product_id')
+        if product_id:
+            product = self.env['product.product'].browse(product_id)
+            res['minimum_qty'] = product.minimum_qty or 1
+
+        return res
